13fbd0dccd6a1ee1145797b453391c815a5a72a5
SOLIDEx21 <164268377+SOLIDEx21@users.noreply.github.com>
2024-11-05 10:17:28 +0400

    Kok Alma Islemleride Ekledim

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,49 +2,59 @@
 #include <cmath> 
 using namespace std;
 
-int main() 
+int main2()
 {
     int a, b;
-    cout << "Ilk Numaryi Gir: ";
-    cin >> a; 
+    cout << "Ilk Numaryi Gir:";
+    cin >> a;
     cout << "Ikinci Numarayi Gir: ";
-    cin >> b; 
+    cin >> b;
 
     char operation;
-    cout << "CHOOSE OPERATION [*, +, -, /,]: ";
-    cin >> operation; 
+    cout << "Yapmak Istediginiz Islemi Secin [*, +, -, /, r,]: ";
+    cin >> operation;
 
-    float result; 
-
-    
+    float result;
     switch (operation) {
-        case '+':
-            result = a + b;
-            cout << "Result: " << result << endl;
-            break;
+    case '+':
+        result = a + b;
+        cout << "Sonuc: " << result << endl;
+        break;
 
-        case '-':
-            result = a - b;
-            cout << "Result: " << result << endl;
-            break; 
+    case '^':
+        result = pow(a, b);
+        cout << "Sonuc: " << result << endl;
+        break;
 
-        case '/':
-            if (b != 0) { 
-                result = static_cast<float>(a) / b; 
-                cout << "Result: " << result << endl;
-            } else {
-                cout << "Error: Division by zero!" << endl; 
-            }
-            break; 
+    case 'ra':
+        result = sqrt(a);
+        cout << "Sonuc: " << result << endl;
+        break;
 
-        case '*':
-            result = a * b; 
-            cout << "Result: " << result << endl;
-            break; 
-        default:
-            cout << "Yanlis Secim!" << endl; 
-            break; 
+    case '-':
+        result = a - b;
+        cout << "Sonuc: " << result << endl;
+        break;
+
+    case '/':
+        if (b != 0) {
+            result = static_cast<float>(a) / b;
+            cout << "Sonuc: " << result << endl;
+        }
+        else {
+            cout << "0 A BOLME ISLEMI YOKTUR!" << endl;
+        }
+        break;
+
+    case '*':
+        result = a * b;
+        cout << "Sonuc: " << result << endl;
+        break;
+
+    default:
+        cout << "Yanlis Secim!" << endl;
+        break;
     }
 
-    return 0; 
+    return 0;
 }
